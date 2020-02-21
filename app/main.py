68407db36a2921b14e4c78779b681fbb379b8679
Alex Lebedev <lebedev.alex.dev@gmail.com>
2020-02-21 12:51:59 +0300

    added exceptions

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from typing import List
 from sqlalchemy.orm import Session
 
@@ -29,11 +29,17 @@ def read_users(skip: int = 0, limit: int = 3, db: Session = Depends(get_db)):
 
 @app.get('/users/{user_id}', response_model=schemas.User)
 def read_user(user_id: int, db: Session = Depends(get_db)):
-    return crud.get_user(db, user_id=user_id)
+    user = crud.get_user(db, user_id=user_id)
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
 
 
 @app.post('/users/', response_model=schemas.User)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    user = crud.get_user_by_email(db, email=user.email)
+    if user:
+        raise HTTPException(status_code=400, detail="Email already registered")
     return crud.create_user(db=db, user=user)
 
 
