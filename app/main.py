5ba09f6106564b466ff14235ec2fd30cbad89a2f
Alex Lebedev <lebedev.alex.dev@gmail.com>
2020-02-21 11:35:34 +0300

    added db, models, endpoints

--- app/main.py
+++ app/main.py
@@ -1,6 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from pydantic import BaseModel
+from sqlalchemy.orm import Session
+
+from . import models
+from .database import SessionLocal, engine
 
 app = FastAPI()
+
+
+class UserCreate(BaseModel):
+    email: str
+    password: str
+
+
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
 
 
 @app.get('/')
@@ -9,5 +27,20 @@ def read_root():
 
 
 @app.get('/users/{user_id}')
-def read_user(user_id: int):
-    return {'id': user_id}
+def read_user(user_id: int, db: Session = Depends(get_db)):
+    return db.query(models.User).first()
+
+
+@app.post('/users/')
+def create_user(user: UserCreate, db: Session = Depends(get_db)):
+    hash_pass = f"fake-{user.password}"
+    new_user = models.User(email=user.email, hashed_password=hash_pass)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+
+@app.on_event("startup")
+def startup():
+    models.Base.metadata.create_all(bind=engine)
